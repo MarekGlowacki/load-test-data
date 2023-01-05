fe8ee2b466cd3371b4da9be69314e385259a0402
User <user@its.me>
2023-01-05 08:59:52 +0100

    Auto load test data to database at start application

---
+++ src/main/java/online/javafun/userbase/LoadTestDataApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.userbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTestDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadTestDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/javafun/userbase/LoadTestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.userbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
